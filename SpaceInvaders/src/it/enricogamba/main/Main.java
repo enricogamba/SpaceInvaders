4021ff517d61750247bd6c6229a2b38972f8524f
enricogamba <32386794+enricogamba@users.noreply.github.com>
2017-10-05 14:32:58 +0200

    First commit

---
+++ SpaceInvaders/src/it/enricogamba/main/Main.java
@@ -0,0 +1,279 @@
+package it.enricogamba.main;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+import it.enricogamba.utils.Utils;
+
+/*
+ * SPACE INVADERS IDENTIFICATION
+ * 
+ * 
+ * PROCEDURE:
+ * 
+ * 1. ask the user for a tolerance value in percentage (in order to recognize space invaders and tolerate false positives / negatives)
+ * 2. read radar image file list
+ * cycle the list:
+ * 3. create a matrix from it
+ * 	3a. the matrix should be larger than the original file in order to consider edge cases
+ * 4. read space invaders images files names
+ * 5. cycle the space invaders images files names list (?or ask the user what image they want to analyze?)
+ * 	5a. create an empty matrix that will be filled with the recognised space invaders
+ * 	5b. read the current space invader file
+ * 	5c. create a matrix from the file
+ * 	5d. cycle the radar image starting from the top left.
+ * 		5d1. For any point extract a submatrix of the same dimensions as the space invader matrix.
+ * 		5d2. compare the submatrix with the space invader's one
+ * 		5d3. if the matrixes are equals (considering the tolerance) the submatrix is accepted and painted to the output matrix
+ * 		5d4. for the moment we don't consider overlapping, so, if the tolerance is satisfied, we jump to the next non-overlapping position (OR NOT???)
+ * 	5e. output:
+ * 		5e1. output image (with false positives / negatives highlighted)
+ * 		5e2. some statistics (number of invaders found, ...)
+ * 
+ * 
+ * COMPROMISES:
+ * - Files must be saved in the defined folders inside the project main folder
+ * - Only for this test, one radar image is allowed (to analyze more images cycling on them will be enaugh)
+ * - Didn't use interface for utilities class
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * paramethers and variables setup
+		 */
+		int tolerance_perc = 0;
+		boolean tolerance_OK = false;
+		
+		final String radarImagesFolderPath = "./RadarImages";
+		final String knownSpaceInvadersImagesPath = "./KnownSpaceInvadersImages";
+		final int radarImageMatrixExtensions = 10; //radar image matrix wil be extended in order to analyze the edge cases
+		final char ext = '*'; //char used to mark extensions
+		final char empty = '.'; //char used to mark empty matrix
+		
+		final boolean DEBUG = false;
+		
+		Scanner reader = new Scanner(System.in);
+		/*
+		 * 1. Read input paramethers
+		 */
+		
+		System.out.println("**************************************************************");
+		System.out.println("Welcome to SPACE INVADERS!");
+		System.out.println("**************************************************************");
+		
+		System.out.println("");
+		System.out.println("In order to proceed with the detection, make sure you put the right files in the right folders:");
+		System.out.println("space invaders images -> ./KnownSpaceInvadersImages");
+		System.out.println("radar image -> ./RadarImages");
+		System.out.println("");
+		System.out.println("May we proceed? (Y/N)");
+		if (!reader.next().trim().equalsIgnoreCase("Y")) {
+			System.out.println("Sad about that. Bye, see you next time!");
+			System.exit(0);
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Ready to detect space invaders. What percentage of tolerance do you want me to use? [0-100]");
+		System.out.println("(0 -> perfect match, 100 -> really???");
+		
+		while (!tolerance_OK) {
+			try {
+				tolerance_perc = reader.nextInt();
+				tolerance_OK = true;
+			} catch (Exception e) {
+				System.out.println("Are you sure you entered a correct integer value? Would you like to try again? (Y/N)");
+				if (!reader.next().trim().equalsIgnoreCase("Y")) {
+					System.out.println("Sad about that. Bye, see you next time!");
+					System.exit(0);
+				}
+			}
+		}
+		
+		
+		/*
+		 * 2. read radar image file(s)
+		 */
+		File radarImagesFolder = new File(radarImagesFolderPath);
+		File[] radarImageFileList = radarImagesFolder.listFiles();
+		for(File radarImageFile : radarImageFileList) {
+			String radarImageFileName = radarImageFile.getName();
+			System.out.println("'"+radarImageFileName + "' found, proceed? [Y/N]");
+			if (!reader.next().trim().equalsIgnoreCase("Y")) {
+				System.out.println("OK. I will skip to the next file if I find one.");
+				continue;
+			}
+			/*
+			 * read file lines into an arraylist in order to have the dimensions and create the matrix
+			 */
+			
+			
+			/*
+			 * 3. create radar image matrix
+			 */
+			char[][] radarImageMatrix = Utils.getMatrixFromFile(radarImageFile, radarImageMatrixExtensions, ext);
+			
+			
+
+
+		    
+		    //print the extended radar image (just for check & fun)
+		    System.out.println("Would you like to see the extended radar image? (Y/N)");
+		    if (reader.next().trim().equalsIgnoreCase("Y")) {
+		    	Utils.printImageMatrix(radarImageMatrix);
+		    }
+
+		    
+		    
+		    /*
+		     * 4. read space invaders image files names
+		     */
+			File knownSpaceInvadersImagesFolder = new File(knownSpaceInvadersImagesPath);
+			File[] knownSpaceInvadersImageFileList = knownSpaceInvadersImagesFolder.listFiles();
+			/*
+			 * 5. cycle space invaders
+			 */
+			for(File knownSpaceInvadersImageFile : knownSpaceInvadersImageFileList) {
+				String knownSpaceInvadersImageFileName = knownSpaceInvadersImageFile.getName();
+				System.out.println("'"+knownSpaceInvadersImageFileName + "' found, proceed? [Y/N]");
+				if (!reader.next().trim().equalsIgnoreCase("Y")) {
+					System.out.println("OK. I will skip to the next file if I find one.");
+					continue;
+				}
+				
+				//read image file and create a matrix
+				
+				/*
+				 * 5a. empty matrix thath will be filled with recognized space invaders
+				 */
+				char[][] recognizedSpaceInvadersImageMatrix = new char[radarImageMatrix.length][radarImageMatrix[0].length];
+				
+				//fill in the empty matrix
+				//first extensions ...
+				for (int y=0; y<recognizedSpaceInvadersImageMatrix.length; y++) {
+					for (int x=0; x<recognizedSpaceInvadersImageMatrix[y].length; x++) {
+						recognizedSpaceInvadersImageMatrix[y][x] = ext;
+					}
+				}
+				///...then empties ...
+				for (int y=radarImageMatrixExtensions; y<recognizedSpaceInvadersImageMatrix.length-radarImageMatrixExtensions; y++) {
+					for (int x=radarImageMatrixExtensions; x<recognizedSpaceInvadersImageMatrix[y].length-radarImageMatrixExtensions; x++) {
+						recognizedSpaceInvadersImageMatrix[y][x] = empty;
+					}
+				}
+				
+				
+				/*
+				 * 	5b. read the current space invader file
+				 * 	5c. create a matrix from the file
+				 */
+				char[][] knownSpaceInvaderMatrix = Utils.getMatrixFromFile(knownSpaceInvadersImageFile, 0, ext);
+				
+				if (knownSpaceInvaderMatrix==null || knownSpaceInvaderMatrix.length < 1) {
+					System.out.println("Matrix is empty. I will step to the next file.");
+					continue;
+				}
+				
+				/*
+				 * 	5d. cycle the radar image starting from the top left.
+				 */
+				int matches_n = 0;
+				for (int y=0; y< radarImageMatrix.length-knownSpaceInvaderMatrix.length; y++) {
+					for (int x=0; x<radarImageMatrix[y].length-knownSpaceInvaderMatrix[0].length; x++) {
+						/* 
+						 * 		5d1. For any point extract a submatrix of the same dimensions as the space invader matrix from the radar image matrix
+						 */
+						char[][] radarImageSubMatrix = new char[knownSpaceInvaderMatrix.length][knownSpaceInvaderMatrix[0].length];
+						int y_s = 0;//submatrix y
+						for (int y_=y; y_< y+knownSpaceInvaderMatrix.length; y_++) {
+							int x_s = 0;//submatrix x
+							for (int x_=x; x_<x+knownSpaceInvaderMatrix[0].length; x_++) {
+								
+								
+								radarImageSubMatrix[y_s][x_s] = radarImageMatrix[y_][x_];
+								
+								x_s++;
+							}//for (int x_=x; x_<x+knownSpaceInvaderMatrix[0].length; x_++) END
+							y_s++;
+						}//for (int y_=y; y_< y+knownSpaceInvaderMatrix.length; y_++) END
+						
+						//System.out.println("TEST SUBMATRIX");
+						//Utils.printImageMatrix(radarImageSubMatrix);
+						
+				/* 
+				* 		5d2. compare the submatrix with the space invader's one
+				*/
+						BigDecimal matchPerc = new BigDecimal(0);
+						
+						matchPerc = Utils.getMatchPerc(knownSpaceInvaderMatrix, radarImageSubMatrix);
+						
+						if (matchPerc.compareTo(new BigDecimal(100-tolerance_perc))>0) {
+							matches_n++;
+							
+							//DEBUG
+							if (DEBUG) {
+								System.out.println("==================================================================================");
+								Utils.printImageMatrix(knownSpaceInvaderMatrix);
+								System.out.println("----------------------------------------------------------------------------------");
+								Utils.printImageMatrix(radarImageSubMatrix);
+								System.out.println("----------------------------------------------------------------------------------");
+								System.out.println(matchPerc);
+								System.out.println("==================================================================================");
+								System.out.println("Proceed?");
+								while (!reader.next().trim().equalsIgnoreCase("Y")) {
+									System.out.println("OK, and now?");	
+								}
+							}
+						
+							/*
+							 * 		5d3. if the matrixes are equals (considering the tolerance) the submatrix is accepted and painted to the output matrix
+							 */
+							for (int y_=y; y_< y+knownSpaceInvaderMatrix.length; y_++) {
+								for (int x_=x; x_<x+knownSpaceInvaderMatrix[0].length; x_++) {
+									recognizedSpaceInvadersImageMatrix[y_][x_] = radarImageMatrix[y_][x_];
+								}//for (int x_=x; x_<x+knownSpaceInvaderMatrix[0].length; x_++) END
+							}//for (int y_=y; y_< y+knownSpaceInvaderMatrix.length; y_++) END
+							
+						
+						
+						}//if (matchPerc.compareTo(new BigDecimal(100-tolerance_perc))>0) END
+						
+						
+						
+				 
+					}//for (int x=0; x<radarImageMatrix[y].length-knownSpaceInvaderMatrix[0].length; x++) END
+				}//for (int y=0; y< radarImageMatrix.length-knownSpaceInvaderMatrix.length; y++) END
+				
+				/*
+				 *  	5e. output:
+				 *
+				 * 		5e1. output image (with false positives / negatives highlighted)
+				 * 		5e2. some statistics (number of invaders found, ...)
+				 */
+				System.out.println("==================================================================================");
+				System.out.println("==================================================================================");
+				Utils.printImageMatrix(recognizedSpaceInvadersImageMatrix);
+				System.out.println("==================================================================================");
+				System.out.println("Using " + tolerance_perc + "% tolerance, " + matches_n + " matches have been found for " + knownSpaceInvadersImageFileName + " in " + radarImageFile.getName());
+				if (tolerance_perc>20) {
+					System.out.println("You have selected a high tolerance value. This can cause overlapped images!!!");
+				}
+				System.out.println("==================================================================================");
+			} //for(File knownSpaceInvadersImageFile : knownSpaceInvadersImageFileList) END
+		    
+		    
+		}//for(File radarImageFile : radarImageFileList) END
+	} //main END
+}
+
+
+
+
+
+
---
+++ SpaceInvaders/src/it/enricogamba/utils/Utils.java
@@ -0,0 +1,147 @@
+package it.enricogamba.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public final class Utils {
+	
+	
+	/*
+	 * given a file path, the file is opened and translated into a matrix (char[][])
+	 */
+	public static char[][] getMatrixFromFilePath(String filePath) {
+		return null;
+	}
+	
+	
+	
+	
+	/*
+	 * given 2 matrixes, return percentage of match (0..100)
+	 */
+	public static BigDecimal getMatchPerc(char[][] a, char[][] b) {
+		try {
+			//if matrixes sizes are different the match is 0
+			if (a.length!=b.length || a[0].length!=b[0].length) return new BigDecimal(0);
+			
+			int tot_matches = 0;
+			int tot = 0;
+			
+			BigDecimal matchPerc = new BigDecimal(0);
+			
+			for (int y=0; y<a.length; y++) {
+				for (int x=0; x<a[y].length; x++) {
+					tot++;
+					if (a[y][x]==b[y][x]) tot_matches++;
+				}
+			}
+			
+			try {
+			matchPerc = (new BigDecimal(tot_matches).divide(new BigDecimal(tot),4,RoundingMode.HALF_UP)).multiply(new BigDecimal(100));
+			matchPerc = matchPerc.setScale(0, RoundingMode.HALF_UP);
+			return matchPerc;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return new BigDecimal(0);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new BigDecimal(0);
+		}
+	}
+	
+	
+	/*
+	 * given a file, returns the matrix representing the images
+	 * params:
+	 * - radarImageFile
+	 * - radarImageMatrixExtensions (to extend the matrix in order to consider edge cases
+	 * - ext: char used to fill extensions
+	 */
+	public static char[][] getMatrixFromFile(File radarImageFile, int radarImageMatrixExtensions, char ext) {
+		System.out.println("Processing " + radarImageFile.getName());
+		HashMap<Integer,String> lines = new HashMap<Integer,String>();
+		Integer line_length = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(radarImageFile))) {
+		    String line;
+		    Integer line_number = 0;
+		    while ((line = br.readLine()) != null) {
+		    	line = line.trim();
+		    	if (line_length<line.length()) line_length = line.length();
+		       lines.put(line_number, line);
+		       line_number++;
+		    }
+		} catch (FileNotFoundException e) {
+			System.out.println("OPS, something went wrong in reading the file ...");
+		} catch (IOException e) {
+			System.out.println("OPS, something went wrong in reading the file ...");
+		}
+		int radarImageMatrixLength = 0;
+		int radarImageMatrixHeight = 0;
+		
+		radarImageMatrixHeight = lines.size();
+		radarImageMatrixLength = line_length; //though the lines are supposed to be the same size, I take the longest for setting the matrix size
+		
+		if (radarImageMatrixHeight<1 || radarImageMatrixLength<1) {
+			System.out.println("May you plese verify if the file is empty and try again?");
+		}
+		
+		char[][] radarImageMatrix = new char[radarImageMatrixHeight+2*radarImageMatrixExtensions][radarImageMatrixLength+2*radarImageMatrixExtensions];
+		
+		//fill in the matrix
+		//begin with the extensions
+		//extension will be marked with the extension char ext
+		for (int y=0; y<radarImageMatrix.length; y++) {
+			for (int x=0; x<radarImageMatrix[y].length; x++) {
+				radarImageMatrix[y][x] = ext;
+			}
+		}
+		
+		
+		//now let's feel the matrix with the image values
+	    @SuppressWarnings("rawtypes")
+		Iterator it = lines.entrySet().iterator();
+	    while (it.hasNext()) {
+	        @SuppressWarnings("rawtypes")
+			HashMap.Entry pair = (HashMap.Entry)it.next();
+	        Integer y = (Integer) pair.getKey();
+	        String line = (String) pair.getValue();
+	        char[] lineCharArray = line.toCharArray();
+	        for (int x=0; x<lineCharArray.length; x++) {
+	        	radarImageMatrix[radarImageMatrixExtensions+y][radarImageMatrixExtensions+x] = lineCharArray[x];
+	        }
+	    }
+		
+		
+		return radarImageMatrix;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * given a matrix, it outputs it
+	 */
+	public static void printImageMatrix(char[][] imageMatrix) {
+	    for (int y=0; y<imageMatrix.length; y++) {
+			for (int x=0; x<imageMatrix[y].length; x++) {
+				System.out.print(imageMatrix[y][x]);
+			}
+			System.out.print(System.getProperty("line.separator"));
+		}
+	}
+}
